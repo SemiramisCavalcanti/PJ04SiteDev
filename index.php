f52006e82836eb4c3b58c397635c6a2dd22f517d
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-05-28 10:21:35 -0300

    inicializando

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <title>LinkBio</title>
+
+    <!-- link css -->
+    <link rel="stylesheet" href="plugins\bootstrap-5.1.3-dist\css\bootstrap.min.css">
+    <link rel="stylesheet" href="plugins\fontawesome-free\css\all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body class="overflow-hidden">
+
+    <!-- site -->
+    <div class="base fixed-top">
+        <hr class="bg-secondary mt-3">
+        <div class="container">
+
+
+
+            <div class="text-center rounded  text-light">
+                <div class=" position-absolute top-0 start-50 translate-middle">
+                    <img src="img\FenixEchoBranca.png" alt="" width="100">
+                    <h1>Botões Site</h1>
+                    <h5>Botões Instagram</h5>
+                    <h5>Botões Linkedin</h5>
+                    <h5>Botões Github</h5>
+                    <h5>Botões whatsapp</h5>
+
+                </div>
+
+                <img></img>
+            </div>
+        </div>
+
+    </div>
+
+    <footer class="fixed-bottom base2">
+
+
+    </footer>
+
+    <!-- links js -->
+    <script src="https://ajax.googleapis.com/ajax/"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="plugins\bootstrap-5.1.3-dist\bootstrap-5.1.3-dist\js\bootstrap.bundle.js"></script>
+    <script src="plugins/js/jquery-3.5.1.min.js"></script>
+
+</body>
+
+</html>
